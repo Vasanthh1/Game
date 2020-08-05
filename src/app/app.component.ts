bf904911d8f1dd47ed132f7cd41a45c6272c4c93
Vasanthh1 <rvasanth453@gmail.com>
2020-08-05 18:23:24 +0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,28 +16,28 @@ export class AppComponent  {
   changeValue(obj1,obj2){
     let pos1 = obj1.innerHTML;
     let pos2 = obj2.innerHTML;
-    obj1.innerHTML = pos2;
+    obj1.innerHTML = pos2; 
     obj2.innerHTML = pos1;  
     this.obj2 = obj1;
   }
-  sendObj(obj1,obj2){
+  sendObj(obj1){
     let pos1 = Number.parseInt(obj1.getAttribute('value'));
     let pos2 = Number.parseInt(this.obj2.getAttribute('value'));
-    console.log(pos1+" "+pos2);
+    //console.log(pos1+" "+pos2);
     if(pos1>3 && (pos1-3 == pos2)){
-      console.log('a');
+      //console.log('a');
       this.changeValue(obj1,this.obj2);
     }
     if(pos1%3!=1 &&(pos1-1 == pos2)){
       this.changeValue(obj1,this.obj2);
-      console.log('b');
+      //console.log('b');
     }
     if(pos1%3!=0 && (pos1+1 ==pos2)){
       this.changeValue(obj1,this.obj2);
-      console.log('c');
+      //console.log('c');
     }
     if((pos1<7) && (pos1+3 ==pos2)){
-      console.log('d');
+      //console.log('d');
       this.changeValue(obj1,this.obj2);
       
     }
